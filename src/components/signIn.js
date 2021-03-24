e840f9eb42f66437c2315e23cb982e1cde208b13
pikupooh <rohitdebnath1813@gmail.com>
2021-03-24 09:58:29 +0530

    added signIn page

---
+++ src/components/signIn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SignIn extends React.Component{
+    render(){
+        return(
+            <div>
+                Sign in page
+            </div>
+        );
+    }
+}
+
+export default SignIn
