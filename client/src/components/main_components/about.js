86706564a9468b51e3c5b055828e7241699c2e19
PratikTamang05 <pratiktamang440@gmail.com>
2021-05-06 20:58:31 +0530

    changed about

--- client/src/components/main_components/about.js
+++ client/src/components/main_components/about.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
-import { Image, Row, Container, Col, Button } from "react-bootstrap";
-import { connect } from "react-redux";
+import { Image, Row, Container, Col, Button,Carousel } from "react-bootstrap";
+import rohit from "./profile_components/rohit.jpeg";
+import pratik from  "./profile_components/pratik.jpg"
+import krishanu from  "./profile_components/krishanu.jpg"
+import shaurya from  "./profile_components/shaurya.jpg"
+import zakaria from  "./profile_components/zakaria.jpg"
 import { bindActionCreators } from "redux";
 import { Link } from 'react-router-dom'
-
 import fetchBlogDataAction from "../../redux/thunk/fetchBlogData";
 import BlogDataComments from "../small_components/blogDataComponent/blogDataComments";
 import PostLikesModal from "../small_components/postLikesModal"
@@ -12,36 +15,62 @@ class About extends React.Component {
   
   render() {
     return(
-      <div>
-        <div className="mx-3 my-3">
-          <h1>About</h1>
-        </div>
-        <div className = "mx-5">
-          <div>
-            <h2>Project</h2>
-            <p className="mx-3 my-3">In today's world artists need not restrict themselves to being a local star. It’s time for stepping up their game and going global.
-              What’s a better way to do so than showcasing your work on a platform that is built specifically for them? Artists can build their work portfolio on our site and share it with the whole wide world.
-              Can’t think about what work to put up? The communities got you covered. With regular blogs, comments, and review of your work. Hear what the world has to say to your masterpiece. 
-              Let your work speak for itself. In a world with too much noise, it’s your work that stands out. People who find it appealing will connect to you after seeing what your show.
-              Maybe you seek popularity, then join now to expose your work to people who share your passion.
-              You appreciate the beauty of the world or the depth in the colours then this is the place for you.</p>
-          </div>
-          <div>
-            <h2>Vision</h2>
-            <p className="mx-3 my-3">Our goal is to acheive world peace. We all are aware the reason behind World War 2. To prevent another calamity like that it is necessary
-              artists are given proper opportunities. As failed artists ourselves who have failed to gain any popularity, we have taken the role of those who
-              guide others to a treasure that we cannot possess.
-            </p>
-          </div>
-          <div>
-            <h2>Team</h2>
-            <Image src="https://i.ibb.co/g9pcTwk/P-20191221-180108.jpg" alt="team photo" fluid className="blog_header_image"/>
-            <p className="mx-3 my-3">We are smart hardworking people and Rohit. All of us have spent time together in the Information Technology course of Jadavpur University.
-              As part of the assignment given to us for the Web Development Lab, we have come together to beat the challenge in front of us.
-              We are Krishanu Sarkar(the drone), Pratik Tamang(the airship), Rohit Debnath(the battleship), Shaurya Prakash Shah(the carrier), Zakaria Helal Arzoo(the helicopter). Also if you are reading this please pray for Krishanu.
-            </p>
-          </div>
-        </div>
+      <div className="mx-3 my-3">
+        <Carousel>
+          <Carousel.Item className ="about_carousel_item">
+            <div className="about_headings">
+            <h2>Build Your Own Portfolio</h2>
+            </div>
+            <img className = "about_carousel_image"alt ="image" src ="https://static.wixstatic.com/media/ea6ac8_460f0c2349c44ac7b7aa7cbec1948d21~mv2.jpg/v1/fill/w_1000,h_572,al_c,q_90,usm_0.66_1.00_0.01/ea6ac8_460f0c2349c44ac7b7aa7cbec1948d21~mv2.jpg"></img>
+          </Carousel.Item>
+          <Carousel.Item className="about_carousel_item">
+          <div className="about_headings">
+            <h2>Read and Upload Mangas</h2>
+            </div>
+          <img className = "about_carousel_image" src="https://static3.cbrimages.com/wordpress/wp-content/uploads/2019/07/Colossal-Titan.jpg" alt="image"/>image
+          </Carousel.Item>
+          <Carousel.Item className="about_carousel_item">
+          <div className="about_headings">
+            <h2>Read and Upload Blogs</h2>
+            </div>
+          <img className = "about_carousel_image" src="https://www.lifewire.com/thmb/qPCEjz9lLrIxqgzP34SxumSPYvQ=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/blogging--woman-reading-blog-887987150-5afa2f65ba61770036427de0.jpg" alt="image"/>image
+          </Carousel.Item>
+          <Carousel.Item className="about_carousel_item">
+        
+          <img className = "about_carousel_image" src="https://www.shortlist.com/media/imager/201907/36842-posts.article_lg.jpg" alt="image"/>image
+          <Carousel.Caption>
+            <h3>Read and upload comics</h3>
+          </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="about_carousel_item">
+          <div className="about_headings">
+            <h2>Find a perfect job </h2>
+            </div>
+          <img className = "about_carousel_image" src="https://abj.artrepreneur.com/wp-content/uploads/2018/03/twenty20_cd7e8978-9870-4b12-b8c8-d6b1a0814600-800x556.jpg" alt="image"/>image
+          </Carousel.Item>
+          <Carousel.Item className="about_carousel_item">
+          <div className="about_headings">
+            <h2>Find an artist who can do your job </h2>
+            </div>
+          <img className = "about_carousel_image" src="https://www.ie.edu/insights/wp-content/uploads/2020/10/Hindi-Art-for-Busniness-Leaders-2048x1152.jpg" alt="image"/>image
+          </Carousel.Item>
+        </Carousel>
+      
+        <h2 className = "front_end">FRONT-END </h2>
+        <Container>
+        <Image src = {rohit} className= "display_pic" fluid ></Image>
+        <Image src = {pratik} className= "display_pic" fluid ></Image>
+        <Image src = {krishanu} className= "display_pic" fluid ></Image>
+        </Container>
+        
+        <h2 className = "front_end">BACK-END </h2>
+        <Container>
+        <Image src = {shaurya} className= "display_pic" fluid ></Image>
+        <Image src = {zakaria} className= "display_pic" fluid ></Image>
+        </Container>
+
+  
+      
       </div>
       )
     }
