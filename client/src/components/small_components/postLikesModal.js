c222628effc2e211eb86eec8cf3af12ded9e1580
shauryashah <shauryapshah@gmail.com>
2021-06-12 19:29:51 +0530

    Fixed like and follow errors.

--- client/src/components/small_components/postLikesModal.js
+++ client/src/components/small_components/postLikesModal.js
@@ -1,28 +1,25 @@
-import { Modal } from 'react-bootstrap'
+import { Modal } from "react-bootstrap";
 
-import PostLikedUser from './postLikedUser'
+import PostLikedUser from "./postLikedUser";
 
-function PostLikesModal(props){
-    if(props.likes.length === 0){
-        return(
-            <Modal show = {props.show} onHide = {props.hideLikesModal} centered>
+function PostLikesModal(props) {
+    if (props.likes.length === 0) {
+        return (
+            <Modal show={props.show} onHide={props.hideLikesModal} centered>
+                <Modal.Body>No Likes</Modal.Body>
+            </Modal>
+        );
+    } else {
+        return (
+            <Modal show={props.show} onHide={props.hideLikesModal} centered>
                 <Modal.Body>
-                    No Likes
+                    {props.likes.map((like) => (
+                        <PostLikedUser like={like} key={like.id} />
+                    ))}
                 </Modal.Body>
             </Modal>
-        )
-    }
-    else{
-        return(
-            <Modal show = {props.show} onHide = {props.hideLikesModal} centered>
-                <Modal.Body>
-                    {props.likes.map((like) => 
-                        <PostLikedUser like = {like} key = {like.userId}/>
-                    )}
-                </Modal.Body>
-            </Modal>
-        )
+        );
     }
 }
 
-export default PostLikesModal
+export default PostLikesModal;
