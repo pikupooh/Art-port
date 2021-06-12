402eb58ef4ed80b8411b1c22ca2844464982e5fd
shauryashah <shauryapshah@gmail.com>
2021-06-12 22:23:01 +0530

    Manga edit and delete button fixed.

--- client/src/components/main_components/mangaData.js
+++ client/src/components/main_components/mangaData.js
@@ -1,133 +1,140 @@
 import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Container, Row, Button} from "react-bootstrap"
+import { Container, Row, Button } from "react-bootstrap";
 
 import MangaDataDisplay from "../small_components/mangaDataDisplayComponents/mangaDataDisplay";
-import MangaDescriptionDisplay from "../small_components/mangaDataDisplayComponents/mangaDescriptionDisplay"
+import MangaDescriptionDisplay from "../small_components/mangaDataDisplayComponents/mangaDescriptionDisplay";
 import fetchMangaDataAction from "../../redux/thunk/fetchMangaData";
-import MangaChaptersDisplay from "../small_components/mangaDataDisplayComponents/mangaChaptersDisplay"
-import MangaDataComments from '../small_components/mangaDataDisplayComponents/comments/mangaDataComments'
-import ChapterModal from "../small_components/uploadForms/chapterModal"
+import MangaChaptersDisplay from "../small_components/mangaDataDisplayComponents/mangaChaptersDisplay";
+import MangaDataComments from "../small_components/mangaDataDisplayComponents/comments/mangaDataComments";
+import ChapterModal from "../small_components/uploadForms/chapterModal";
 import ShareRow from "../small_components/ShareRow";
-import {fetchUserProfileData} from "../../redux/thunk/fetchProfileData"
-import SingleTag from "../small_components/singleTag"
-import deleteLastChapter from "../../redux/thunk/delete/deleteLastChapter"
-import { SHOW_DELETE_MODAL } from "../../redux/actions/actionTypes"
+import { fetchUserProfileData } from "../../redux/thunk/fetchProfileData";
+import SingleTag from "../small_components/singleTag";
+import deleteLastChapter from "../../redux/thunk/delete/deleteLastChapter";
+import { SHOW_DELETE_MODAL } from "../../redux/actions/actionTypes";
 class MangaData extends React.Component {
+    constructor(props) {
+        super(props);
 
-  constructor(props) {
-      super(props);
-
-      this.state = {
-          formShow: false,
-      };
-  }
+        this.state = {
+            formShow: false,
+        };
+    }
     componentDidMount() {
-      let mangaId = this.props.location.pathname.slice(8);
-      this.props.fetchMangaData(mangaId)
-      window.scrollTo(0, 0);
+        let mangaId = this.props.location.pathname.slice(8);
+        this.props.fetchMangaData(mangaId);
+        window.scrollTo(0, 0);
     }
 
     handleFormModalClose = () => {
-      this.setState({ formShow: false });
+        this.setState({ formShow: false });
     };
 
     handleFormModalShow = () => {
-      this.setState({ formShow: true });
+        this.setState({ formShow: true });
     };
 
     deleteChapter = () => {
-      this.props.deleteLastChapter(this.props.mangaId)
-    }
+        this.props.deleteLastChapter(this.props.mangaId);
+    };
 
     render() {
-      return(
-        <div>
-          <Container>
-            <MangaDataDisplay photo={this.props.coverPhoto} 
-                             rating ={this.props.rating}
-                             ratingsCount={this.props.ratingsCount}
-                             type ={this.props.type} 
-                             author= {this.props.author}
-                             title = {this.props.title}
-                             chaptersLength = {this.props.chapters.length}
-                             userRatings = {this.props.userRatings} 
-                             mangaId = {this.props.mangaId}
-                             handleFormModalShow = {this.handleFormModalShow}
-                             userId = {this.props.userId}
-                             />
+        return (
+            <div>
+                <Container>
+                    <MangaDataDisplay
+                        photo={this.props.coverPhoto}
+                        rating={this.props.rating}
+                        ratingsCount={this.props.ratingsCount}
+                        type={this.props.type}
+                        author={this.props.author}
+                        title={this.props.title}
+                        chaptersLength={this.props.chapters.length}
+                        userRatings={this.props.userRatings}
+                        mangaId={this.props.mangaId}
+                        handleFormModalShow={this.handleFormModalShow}
+                        userId={this.props.userId}
+                    />
 
-            <MangaDescriptionDisplay about = {this.props.about}/>
-            <MangaChaptersDisplay chapters ={this.props.chapters}/>
-            {this.props.userId === this.props.author.userId && this.props.chapters.length !== 0 &&
-              <div className = "delete_button_container">
-                <Button id = "delete_chapter_button" onClick = {() => this.props.showDeleteModal(this.deleteChapter)}>
-                    Delete last chapter
-                </Button>
-              </div>
-            } 
-            <Row className = "mx-2 tags_text" id = "tags">
-              <i class="material-icons">
-                loyalty
-              </i>
-              Tags
-            </Row>
-            <Row className = "zeromargin"> 
-              {this.props.tags.map((tag) =>
-                <SingleTag tag = {tag} />
-              )}
-            </Row>
-            <ShareRow />
-            <MangaDataComments comments = {this.props.comments} mangaId = {this.props.mangaId}/>
-          </Container>
-          <ChapterModal
+                    <MangaDescriptionDisplay about={this.props.about} />
+                    <MangaChaptersDisplay chapters={this.props.chapters} />
+                    {this.props.userId === this.props.author.id &&
+                        this.props.chapters.length !== 0 && (
+                            <div className="delete_button_container">
+                                <Button
+                                    id="delete_chapter_button"
+                                    onClick={() =>
+                                        this.props.showDeleteModal(
+                                            this.deleteChapter
+                                        )
+                                    }
+                                >
+                                    Delete last chapter
+                                </Button>
+                            </div>
+                        )}
+                    <Row className="mx-2 tags_text" id="tags">
+                        <i class="material-icons">loyalty</i>
+                        Tags
+                    </Row>
+                    <Row className="zeromargin">
+                        {this.props.tags.map((tag) => (
+                            <SingleTag tag={tag} />
+                        ))}
+                    </Row>
+                    <ShareRow />
+                    <MangaDataComments
+                        comments={this.props.comments}
+                        mangaId={this.props.mangaId}
+                    />
+                </Container>
+                <ChapterModal
                     show={this.state.formShow}
                     handleModalClose={this.handleFormModalClose}
                     mangaOrComic={"MANGA"}
                     mangaId={this.props.mangaId}
                 />
-        </div>
-      )
+            </div>
+        );
     }
-  }
-  const mapDispatchToProps = (dispatch) =>
-  bindActionCreators(
-    {
-      fetchMangaData: fetchMangaDataAction,
-      fetchUserProfile: fetchUserProfileData,
-      deleteLastChapter,
-      showDeleteModal: (deleteFunction) => 
-            dispatch({
-                type: SHOW_DELETE_MODAL,
-                payload: {
-                    message: "Last Chapter",
-                    delete: deleteFunction
-                }
-            })
-    },
-    dispatch
-  );
+}
+const mapDispatchToProps = (dispatch) =>
+    bindActionCreators(
+        {
+            fetchMangaData: fetchMangaDataAction,
+            fetchUserProfile: fetchUserProfileData,
+            deleteLastChapter,
+            showDeleteModal: (deleteFunction) =>
+                dispatch({
+                    type: SHOW_DELETE_MODAL,
+                    payload: {
+                        message: "Last Chapter",
+                        delete: deleteFunction,
+                    },
+                }),
+        },
+        dispatch
+    );
 
 const mapStateToProps = (state) => {
-  
-  return {
-    userId: state.auth.userId,
-    mangaId: state.mangaData.id,
-    chapters: state.mangaData.chapters,
-    rating: state.mangaData.rating,
-    ratingsCount: state.mangaData.ratingsCount,
-    title: state.mangaData.title,
-    about: state.mangaData.about,
-    type: state.mangaData.type,
-    coverPhoto: state.mangaData.coverPhoto,
-    author : state.mangaData.author,
-    comments : state.mangaData.comments,
-    uploadDate : state.mangaData.uploadDate,
-    tags : state.mangaData.tags,
-    userRatings: state.auth.profile.userRatings
-  };
+    return {
+        userId: state.auth.userId,
+        mangaId: state.mangaData.id,
+        chapters: state.mangaData.chapters,
+        rating: state.mangaData.rating,
+        ratingsCount: state.mangaData.ratingsCount,
+        title: state.mangaData.title,
+        about: state.mangaData.about,
+        type: state.mangaData.type,
+        coverPhoto: state.mangaData.coverPhoto,
+        author: state.mangaData.author,
+        comments: state.mangaData.comments,
+        uploadDate: state.mangaData.uploadDate,
+        tags: state.mangaData.tags,
+        userRatings: state.auth.profile.userRatings,
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MangaData);
-  
